6da733640380e878660c13a9674a567de43e5b0e
BobbyDMullinsIII <bobbymullins20000@gmail.com>
2023-03-12 04:56:35 -0400

    Created initial java project

---
+++ JavaSolutionTestingProject/src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * JavaSolutionTestingProject
+ * Main.java
+ * Main driver for program
+ * @author      Bobby Mullins
+ * @since       Sunday, March 12, 2023 (2023-03-12)
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+    }
+}
